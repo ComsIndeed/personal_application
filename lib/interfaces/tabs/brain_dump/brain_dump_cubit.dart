f1efc4f5a4beb1ac6373a2befbb6e91bcadd7921
Coms <125128463+ComsIndeed@users.noreply.github.com>
2026-04-17 19:38:05 +0800

    Initialized brain dump cubit

--- lib/interfaces/tabs/brain_dump/brain_dump_cubit.dart
+++ lib/interfaces/tabs/brain_dump/brain_dump_cubit.dart
@@ -4,28 +4,24 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 class BrainDumpCubit extends Cubit<BrainDumpState> {
   BrainDumpCubit() : super(BrainDumpState());
 
-  void updateTextInput(String text) {
-    emit(state.copyWith(textInput: text));
-  }
-
   void updateFiles(List<PlatformFile> files) {
     emit(state.copyWith(files: files));
   }
+
+  void sendRaw() {}
+
+  void sendProcessed() {}
 }
 
 class BrainDumpState {
   // Inputs
-  final String textInput;
   final List<PlatformFile> files;
 
   // View
 
-  BrainDumpState({this.textInput = '', this.files = const []});
+  BrainDumpState({this.files = const []});
 
-  BrainDumpState copyWith({String? textInput, List<PlatformFile>? files}) {
-    return BrainDumpState(
-      textInput: textInput ?? this.textInput,
-      files: files ?? this.files,
-    );
+  BrainDumpState copyWith({List<PlatformFile>? files}) {
+    return BrainDumpState(files: files ?? this.files);
   }
 }
