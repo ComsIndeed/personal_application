0d7f137aeb4c1448b63fb2da94759cce35de648f
Coms <125128463+ComsIndeed@users.noreply.github.com>
2026-04-23 00:31:40 +0800

    [BUILD] Added back the darker background for more contrast

--- lib/core/widgets/interface_container.dart
+++ lib/core/widgets/interface_container.dart
@@ -271,8 +271,8 @@ class GlassContainer extends StatelessWidget {
                   color:
                       color ??
                       (isDark
-                          ? Colors.black.withValues(alpha: 0.4)
-                          : Colors.white.withValues(alpha: 0.4)),
+                          ? Colors.black.withValues(alpha: 0.8)
+                          : Colors.white.withValues(alpha: 0.8)),
                   border: Border.fromBorderSide(
                     border ??
                         BorderSide(
